5a70f700903b2eb2afa7efe0ae1862d91a73eddf
Nicolas <rojasariasnicolas@gmail.com>
2024-11-13 16:29:17 -0300

    Fin de la seccion 5

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -8,7 +8,21 @@ import { DbzService } from '../services/dbz.service';
 })
 
 export class MainPageComponent  {
-  constructor(public dbzService: DbzService){
+  constructor(private dbzService: DbzService){}
+
+    get character(): Character[] {
+      //Cuando un servicio es privado es necesario sacarlo de ahi para usarlo
+      return [...this.dbzService.character];
+    }
+
+    onDeleteCharacter( id: string): void {
+      this.dbzService.deleteCharacterById( id);
+    }
+
+    onNewCharacter( character: Character): void{
+      this.dbzService.onNewCharacter(character);
+    }
+
 
   }
-}
+
